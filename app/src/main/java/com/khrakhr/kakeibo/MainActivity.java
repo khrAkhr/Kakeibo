3a54bec07abb31840ad9ee3152fd07cf90fbb4be
khrAkhr <khr.sp.ratchico@gmail.com>
2020-08-10 21:54:24 +0900

    ボタンで画面遷移と設定画面のはりぼて

--- app/src/main/java/com/khrakhr/kakeibo/MainActivity.java
+++ app/src/main/java/com/khrakhr/kakeibo/MainActivity.java
@@ -1,14 +1,44 @@
 package com.khrakhr.kakeibo;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.view.View;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.NextMonthBtn).setOnClickListener(this);
+        findViewById(R.id.PrevMonthBtn).setOnClickListener(this);
+        findViewById(R.id.SettingsBtn).setOnClickListener(this);
     }
+
+    public void onClick(View view){
+        if (view != null) {
+            Intent intent;
+            switch (view.getId()) {
+                case R.id.NextMonthBtn:
+                    intent = new Intent(this, MainActivity2.class);
+                    startActivity(intent);
+                    break;
+
+                case R.id.PrevMonthBtn:
+                    intent = new Intent(this, MainActivity2.class);
+                    startActivity(intent);
+                    break;
+
+                case R.id.SettingsBtn:
+                    intent = new Intent(this, SettingsActivity.class);
+                    startActivity(intent);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
 }
